96282e3ef21d092b99973342ec212d8cfbb427ae
Bryan Anthonio <dev@bryananthonio.com>
2023-07-14 10:46:11 -0700

    feat(frontend): added drag and drop using Sortable.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,15 +18,36 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Sortable from "../vendor/sortable"
 
+let Hooks = {}
+
+Hooks.Sortable = {
+    mounted() {
+        console.log("mounted")
+        let sorter = new Sortable(this.el, {
+            animation: 150,
+            delay: 100,
+            dragClass: "drag-item",
+            ghostClass: "drag-ghost",
+            forceFallback: true,
+            onEnd: e => {
+                let params = { old: e.oldIndex, new: e.newIndex, ...e.item.dataset }
+                this.pushEventTo(this.el, "reposition", params)
+            }
+        })
+    }
+}
+
+console.log("here!")
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
