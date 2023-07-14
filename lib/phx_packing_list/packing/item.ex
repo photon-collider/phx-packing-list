31c592eaf6af2e9c47c565091f3763b555305884
Bryan Anthonio <dev@bryananthonio.com>
2023-07-14 09:01:41 -0700

    refactor: updated item changeset to require only packing list id

--- lib/phx_packing_list/packing/item.ex
+++ lib/phx_packing_list/packing/item.ex
@@ -1,21 +1,21 @@
 defmodule PhxPackingList.Packing.Item do
   use Ecto.Schema
   import Ecto.Changeset
+  alias PhxPackingList.Packing.PackingList
 
   schema "items" do
     field :name, :string
     field :notes, :string
     field :quantity, :integer, default: 1
     field :packed, :boolean, default: false
-    belongs_to :packing_list, PhxPackingList.Packing.PackingList
-
+    belongs_to :packing_list, PackingList
     timestamps()
   end
 
   @doc false
   def changeset(item, attrs) do
     item
-    |> cast(attrs, [:name, :quantity, :notes])
-    |> validate_required([:name, :quantity, :notes])
+    |> cast(attrs, [:name, :notes, :packed, :packing_list_id])
+    |> validate_required([:packing_list_id])
   end
 end
