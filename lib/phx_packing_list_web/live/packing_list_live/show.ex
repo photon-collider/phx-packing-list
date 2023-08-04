14dba0b9a7d79e61eadb027fbfa1218f98b8a5e8
Bryan Anthonio <dev@bryananthonio.com>
2023-08-04 07:20:39 -0700

    feat(packing-lists): restyled packing list menu and added form for creating new items

--- lib/phx_packing_list_web/live/packing_list_live/show.ex
+++ lib/phx_packing_list_web/live/packing_list_live/show.ex
@@ -2,6 +2,7 @@ defmodule PhxPackingListWeb.PackingListLive.Show do
   use PhxPackingListWeb, :live_view
   alias PhxPackingList.Packing
   alias PhxPackingListWeb.ItemLive
+  alias PhxPackingList.Packing.Item
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,7 +16,31 @@ defmodule PhxPackingListWeb.PackingListLive.Show do
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:packing_list_id, id)
      |> assign(:packing_list, Packing.get_packing_list!(id))
+     |> assign_changeset()
      |> stream(:items, Packing.list_items_for_packing_list(id))}
+  end
+
+  defp assign_changeset(%{assigns: %{packing_list_id: packing_list_id}} = socket) do
+    assign(
+      socket,
+      :changeset,
+      Packing.change_item(%Item{position: 0, packing_list_id: packing_list_id})
+    )
+  end
+
+  @impl true
+  def handle_event(
+        "crato",
+        %{"item" => item_params},
+        socket
+      ) do
+    IO.inspect(item_params)
+
+    {:ok, item} = Packing.create_item(item_params)
+
+    {:noreply,
+     socket
+     |> stream_insert(:items, item, at: 0)}
   end
 
   @impl true
@@ -25,7 +50,7 @@ defmodule PhxPackingListWeb.PackingListLive.Show do
         %{assigns: %{packing_list_id: packing_list_id}} = socket
       ) do
     # create new item in DB
-    {:ok, item} = Packing.create_item(%{packing_list_id: packing_list_id})
+    {:ok, item} = Packing.create_item(%{packing_list_id: packing_list_id, position: 0})
 
     # add to streams
     {:noreply,
@@ -35,7 +60,7 @@ defmodule PhxPackingListWeb.PackingListLive.Show do
   end
 
   @impl true
-  def handle_event("reposition", %{"listIDs" => list_ids} = _params, socket) do
+  def handle_event("reposition", %{"listIDs" => list_ids}, socket) do
     Enum.with_index(list_ids)
     |> Enum.each(fn {"items-" <> item_id, position} ->
       Packing.get_item!(item_id)
@@ -57,13 +82,11 @@ defmodule PhxPackingListWeb.PackingListLive.Show do
 
   defp handle_updated_item(socket, item) do
     socket
-    |> put_flash(:info, "Item updated!")
     |> stream_insert(:items, item)
   end
 
   defp handle_deleted_item(socket, item) do
     socket
-    |> put_flash(:info, "Item deleted!")
     |> stream_delete(:items, item)
   end
 
